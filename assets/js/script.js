b7073a1336710cdfd5dd3a1542f6a4bb1ed9c20e
Ryan Hsiao <rdjr.hsiao@gmail.com>
2021-09-25 13:35:16 -0700

    inital planning for html css js

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// timer starting from 90 seconds counting donw to 0
+// everytime user gets question wrong, cuts timer down 10 seconds
+// each question switches (maybe carrosel)
+// right and wrong alerts at bottom
+// knowing when its right and wrong.
+// display score at end (which is remaining timer)
+// place to put initals (form)
+//highscore page with drop down list (todo)
+//clear highscore button to clear (local storage)
+// play again button at the end (eventlistener)
