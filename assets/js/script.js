19a9091009e3b568a66bbde1a30ea62b5c7de697
Ryan Hsiao <rdjr.hsiao@gmail.com>
2021-09-26 19:00:13 -0700

    added counter and intial css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,21 @@
+var timeEl = document.querySelector(".timer-count");
+var secondsLeft = 90;
+
 // timer starting from 90 seconds counting donw to 0
+var timerInterval = setInterval(function () {
+    if (secondsLeft <= 0) {
+      // Stops execution of action at set interval
+      clearInterval(timerInterval);
+      // Calls function to go to end screen
+    } else {
+      timeEl.textContent = secondsLeft + ' seconds remaining';
+      secondsLeft--;
+    }
+  }, 1000);
+
+
+
+
 // everytime user gets question wrong, cuts timer down 10 seconds
 // each question switches (maybe carrosel)
 // right and wrong alerts at bottom
